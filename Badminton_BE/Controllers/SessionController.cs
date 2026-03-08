ca7b2e3d39f48c18cb876c103fea92652d9a0918
Tung Nguyen <tung.nguyen@totalsynergy.com>
2026-03-08 15:25:12 +0700

    feat : add sesion controller

---
+++ Badminton_BE/Controllers/SessionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Badminton_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        public SessionController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateSession()
+        {
+            return Ok("Session created successfully");
+        }
+
+        [HttpGet]
+        public IActionResult GetSessions()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetSessionById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateSession(int id)
+        {
+            return Ok();
+        }
+    }
+}
